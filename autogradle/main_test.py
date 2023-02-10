b2ea1ae56b72c4de24585cc01b774d7fd80cd738
Guilherme Salustiano <guissalustiano@gmail.com>
2023-02-10 01:58:04 -0300

    add test to question 2

--- autogradle/main_test.py
+++ autogradle/main_test.py
@@ -174,6 +174,73 @@ def test_1_4():
     assert mvn_output == 3, \
             f"Seu código não está correto"
 
+def test_2():
+    filecode = submission_path / "op-mnem.mvn"
+    assert filecode.exists(), f"A submissão não contém o arquivo '{filecode.name}'"
+
+    input_file = tempfile.NamedTemporaryFile(mode='w')
+    '''
+    # Escrever teste em assembly devia pagar adicional de insalubridade
+    input_file.writelines("""
+0000 0300
+
+; Testa OP2MNEM e MNEM2OP
+;;
+02FC 0000 ; OP2MNEM funcionou?
+02FE 0000 ; MNEM2OP funcionou?
+;;
+0300 8410 ; Carrega OPCODE de JZ
+0302 9010 ; Escreve no operador de OPCODE
+0304 A100 ; Chama OP2MNEM
+0306 8412 ; Carrega MNEM de JZ
+0308 5012 ; Subtrai MNEM de JZ do resultado de OP2MNEM
+030A 130E ; Se for igual, sinaliza que funcionou
+030C 0312 ; Senão, continua
+030E 3001 ; AC <= 1
+0310 92FC ; Sinaliza que OP2MNEM funcionou
+0312 3000 ; AC <= 0
+0314 9010 ; OPCODE <= 0
+0316 9012 ; MNEM <= 0
+0318 8412 ; Carrega MNEM de JZ
+031A 9012 ; Escreve no operador de MNEM
+031C A200 ; Chama MNEM2OP
+031E 8410 ; Carrega OPCODE de JZ
+0320 5010 ; Subtrai OPCODE de JZ do resultado de MNEM2OP
+0322 1326 ; Se for igual, sinaliza que funcionou
+0324 032A ; Senão, finaliza MAIN
+0326 3001 ; AC <= 1
+0328 92FE ; Sinaliza que MNEM2OP funcionou
+032A C32A ; HALT
+    """)
+    input_file.flush()
+    '''
+
+    output_file = tempfile.NamedTemporaryFile(mode='r')
+
+    inputs = [
+        f"p {filecode.as_posix()}",
+        "",
+        f"p {input_file.name}",
+        "",
+        "r",
+        "0",
+        "y",
+        "",
+        f"m 02FC 02FF {output_file.name}",
+        "",
+        "x",
+        "",
+    ]
+
+    out = run_mvn('\n'.join(inputs))
+
+    mvn_output = output_file.read().lstrip('02f0:').strip()
+
+    print(mvn_output)
+
+    assert mvn_output == "00  01  00  01", \
+            f"Seu código não está correto"
+
 def test_3():
     filecode = submission_path / "quadrados-perfeitos.mvn"
     assert filecode.exists(), f"A submissão não contém o arquivo '{filecode.name}'"
