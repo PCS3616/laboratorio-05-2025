f31a9e49a40876194f2675036a2c4b923191387b
Joao-Pedro-Cabral <miranda.jp@usp.br>
2023-12-27 22:04:29 -0300

    Versao 2024

--- README.md
+++ README.md
@@ -1,53 +1,9 @@
-# PCS3616 - Laboratório 5 - MVN 2
+# PCS3616 - Laboratório 5 - MVN 3
 
 Nesta aula vamos continuar o trabalho da semana passada, hoje você deve
 criar mais alguns programas em linguagem de máquina de MVN.
 
-## 1. `triangulos.mvn`
-Escrever um programa que determina, para três valores a, b e c se
-estes valores: não formam um triângulo, formam um triângulo
-retângulo, triângulo acutângulo ou triângulo obtusângulo (valores 0,
-1, 2, 3, respectivamente). Usar as posições 0x010, 0x012, 0x014 para
-os valores a, b e c e a posição 0x016 para o resultado.
-
-
-## 2. `op-mnem.mvn`
-Desenvolva duas sub-rotinas (`OP2MNEM` e `MNEM2OP`), cujas finalidades são:
-
-### OP2MNEM (endereço inicial: 0x100)
-converte um número inteiro dado, 0≤ n ≤ 15, localizado na posição de 
-memória OPCODE (0x010) no mnemônico correspondente, formado por dois
-caracteres ASCII (consultar a tabela de mnemônicos fornecida adiante).
-O mnemônico deverá ser armazenado na posição de memória MNEM (0x012).
-
-### MNEM2OP (endereço inicial: 0x200)
-faz a conversão oposta,
-transformando um mnemônico válido localizado em MNEM, dado como dois
-caracteres ASCII, em um número inteiro correspondente, , 0 ≤ n ≤ 15,
-armazenado na posição de memória OPCODE, novamente conforme a tabela
-de mnemônicos fornecida.
-
-### Um pequeno programa principal (endereço inicial: 0x300)
-deve ilustrar o uso das duas sub-rotinas.
-
-Observação: ambos os parâmetros, MNEM e OPCODE, são representados como
-inteiros, ocupando, cada qual, dois bytes de memória.
-
-Exemplo de operação:
-
--   Dado o OPCODE 0x0001, armazenado na posição OPCODE e OPCODE + 1 de
-    memória, contendo respectivamente os bytes 00 e 01, a sub-rotina
-    OP2MNEM retorna como resultado o par de letras JZ (*jump if zero*),
-    cujos códigos ASCII são 4A e 5A, respectivamente. Em outras
-    palavras, a posição de memória MNEM deverá conter o byte 0x4A e a
-    posição MNEM+1 deverá conter o byte 0x5A.
-
--   Dado o mnemônico JZ em (MNEM, MNEM+1), ou seja, dado o par de bytes
-    (0x4A, 0x5A), a sub-rotina MNEM2OP retornará em (OPCODE, OPCODE+1) o
-    par de bytes (00, 01).
-
-
-## 3. quadrados-perfeitos.mvn
+## 1. `quadrados-perfeitos.mvn`
 Desenvolva um programa para a MVN que calcule e tabele, a partir da
 posição de memória 0x100, os quadrados perfeitos dos 64 primeiros
 números naturais (0x0, 0x1, 0x2, ..., 0x3F). O cálculo deverá ser
@@ -76,3 +32,29 @@ módulo "mvnMonitor.py".
 \*:mostração intuitiva da fórmula
 
 ![](./media/image1.png)
+
+## 2. `io.mvn`
+Escrever um programa que lê dois números do teclado
+(`x` e `y`), e imprime o valor de `x+y`. Observações:
+
+- `0 <= x`, `y <= 99`
+
+- Os números devem ser lidos do teclado, em uma única linha, no
+formato `<x-d1><x-d2><s><s><y-d1><y-d2>`, onde:
+
+  - `<x-d1>` é o primeiro dígito de x. Se `x < 10`, o dígito
+  informado deve ser `0`.
+
+  - `<x-d2>` é o segundo dígito de `x`.
+
+  - `<s>` é um espaço em branco
+
+  - `<y-d1>` é o primeiro dígito de `y`. Se `y < 10`, o dígito
+  informado deve ser `0`.
+
+  - `<x-d2>` é o segundo dígito de `y`.
+
+Por exemplo, \"07 54\" é uma entrada válida, e o programa deve imprimir
+\"61\" na saída.
+
+OBS.: Consulte a [tabela ASCII](http://ascii.cl/)
--- autogradle/main_test.py
+++ autogradle/main_test.py
@@ -5,11 +5,6 @@ import tempfile
 
 submission_path = Path("./submission")
 
-def limpa(string):
-    res=string.split(" ")
-    res=list(filter(None, res))
-    return int(res[1]+res[2],16)
-
 def run_mvn(input_text):
     # I hate the current MVN
     # A good class solve this, but now are a really mess code
@@ -26,222 +21,7 @@ def run_mvn(input_text):
     )
     return p.stdout
 
-def test_1_1():
-    filecode = submission_path / "triangulos.mvn"
-    assert filecode.exists(), f"A submissão não contém o arquivo '{filecode.name}'"
-
-    input_file = tempfile.NamedTemporaryFile(mode='w')
-    input_file.writelines([
-        "0010	0001\n",
-        "0012	0002\n",
-        "0014	0003\n",
-        ";0016	0000\n",
-    ])
-    input_file.flush()
-
-    output_file = tempfile.NamedTemporaryFile(mode='r')
-
-    inputs = [
-        f"p {filecode.as_posix()}",
-        "",
-        f"p {input_file.name}",
-        "",
-        "r",
-        "0",
-        "n",
-        "",
-        f"m 0016 0017 {output_file.name}",
-        "",
-        "x",
-        "",
-    ]
-
-    out = run_mvn('\n'.join(inputs))
-
-    mvn_output = limpa(output_file.read())
-
-    assert mvn_output == 0, \
-            f"Seu código não está correto"
-
-def test_1_2():
-    filecode = submission_path / "triangulos.mvn"
-    assert filecode.exists(), f"A submissão não contém o arquivo '{filecode.name}'"
-
-    input_file = tempfile.NamedTemporaryFile(mode='w')
-    input_file.writelines([
-        "0010	0003\n",
-        "0012	0004\n",
-        "0014	0005\n",
-        ";0016	0001\n",
-    ])
-    input_file.flush()
-
-    output_file = tempfile.NamedTemporaryFile(mode='r')
-
-    inputs = [
-        f"p {filecode.as_posix()}",
-        "",
-        f"p {input_file.name}",
-        "",
-        "r",
-        "0",
-        "n",
-        "",
-        f"m 0016 0017 {output_file.name}",
-        "",
-        "x",
-        "",
-    ]
-
-    out = run_mvn('\n'.join(inputs))
-
-    mvn_output = limpa(output_file.read())
-
-    assert mvn_output == 1, \
-            f"Seu código não está correto"
-
-def test_1_3():
-    filecode = submission_path / "triangulos.mvn"
-    assert filecode.exists(), f"A submissão não contém o arquivo '{filecode.name}'"
-
-    input_file = tempfile.NamedTemporaryFile(mode='w')
-    input_file.writelines([
-        "0010	0003\n",
-        "0012	0004\n",
-        "0014	0004\n",
-        ";0016	0002\n",
-    ])
-    input_file.flush()
-
-    output_file = tempfile.NamedTemporaryFile(mode='r')
-
-    inputs = [
-        f"p {filecode.as_posix()}",
-        "",
-        f"p {input_file.name}",
-        "",
-        "r",
-        "0",
-        "n",
-        "",
-        f"m 0016 0017 {output_file.name}",
-        "",
-        "x",
-        "",
-    ]
-
-    out = run_mvn('\n'.join(inputs))
-
-    mvn_output = limpa(output_file.read())
-
-    assert mvn_output == 2, \
-            f"Seu código não está correto"
-
-def test_1_4():
-    filecode = submission_path / "triangulos.mvn"
-    assert filecode.exists(), f"A submissão não contém o arquivo '{filecode.name}'"
-
-    input_file = tempfile.NamedTemporaryFile(mode='w')
-    input_file.writelines([
-        "0010	0003\n",
-        "0012	0004\n",
-        "0014	0006\n",
-        ";0016	0003\n",
-    ])
-    input_file.flush()
-
-    output_file = tempfile.NamedTemporaryFile(mode='r')
-
-    inputs = [
-        f"p {filecode.as_posix()}",
-        "",
-        f"p {input_file.name}",
-        "",
-        "r",
-        "0",
-        "n",
-        "",
-        f"m 0016 0017 {output_file.name}",
-        "",
-        "x",
-        "",
-    ]
-
-    out = run_mvn('\n'.join(inputs))
-
-    mvn_output = limpa(output_file.read())
-
-    assert mvn_output == 3, \
-            f"Seu código não está correto"
-
-def test_2():
-    filecode = submission_path / "op-mnem.mvn"
-    assert filecode.exists(), f"A submissão não contém o arquivo '{filecode.name}'"
-
-    input_file = tempfile.NamedTemporaryFile(mode='w')
-    input_file.writelines("""
-0000 0300
-
-; Testa OP2MNEM e MNEM2OP
-;;
-02FC 0000 ; OP2MNEM funcionou?
-02FE 0000 ; MNEM2OP funcionou?
-;;
-0300 8410 ; Carrega OPCODE de JZ
-0302 9010 ; Escreve no operador de OPCODE
-0304 A100 ; Chama OP2MNEM
-0306 8412 ; Carrega MNEM de JZ
-0308 5012 ; Subtrai MNEM de JZ do resultado de OP2MNEM
-030A 130E ; Se for igual, sinaliza que funcionou
-030C 0312 ; Senão, continua
-030E 3001 ; AC <= 1
-0310 92FC ; Sinaliza que OP2MNEM funcionou
-0312 3000 ; AC <= 0
-0314 9010 ; OPCODE <= 0
-0316 9012 ; MNEM <= 0
-0318 8412 ; Carrega MNEM de JZ
-031A 9012 ; Escreve no operador de MNEM
-031C A200 ; Chama MNEM2OP
-031E 8410 ; Carrega OPCODE de JZ
-0320 5010 ; Subtrai OPCODE de JZ do resultado de MNEM2OP
-0322 1326 ; Se for igual, sinaliza que funcionou
-0324 032A ; Senão, finaliza MAIN
-0326 3001 ; AC <= 1
-0328 92FE ; Sinaliza que MNEM2OP funcionou
-032A C32A ; HALT
-
-0410 0001
-0412 4A5A
-    """)
-    input_file.flush()
-
-    output_file = tempfile.NamedTemporaryFile(mode='r')
-
-    inputs = [
-        f"p {filecode.as_posix()}",
-        "",
-        f"p {input_file.name}",
-        "",
-        "r",
-        "0",
-        "y",
-        "",
-        f"m 02FC 02FF {output_file.name}",
-        "",
-        "x",
-        "",
-    ]
-
-    out = run_mvn('\n'.join(inputs))
-
-    mvn_output = output_file.read().lstrip('02f0:').strip()
-
-    print(mvn_output)
-
-    assert mvn_output == "00  01  00  01", \
-            f"Seu código não está correto"
-
-def test_3():
+def test_1():
     filecode = submission_path / "quadrados-perfeitos.mvn"
     assert filecode.exists(), f"A submissão não contém o arquivo '{filecode.name}'"
 
@@ -275,3 +55,37 @@ Final do dump.""".strip()
 
     assert mvn_output == expected_output, \
             f"Seu código não está correto"
+
+def test_2():
+    filecode = submission_path / "io.mvn"
+    assert filecode.exists(), f"A submissão não contém o arquivo '{filecode.name}'"
+
+    newcodefile = tempfile.NamedTemporaryFile(mode='w')
+
+    with open(filecode) as f:
+        new=re.sub("(e|E)100", "e300", f.read())
+        newcodefile.write(new)
+        newcodefile.flush()
+
+    outputfile = tempfile.NamedTemporaryFile(mode='r')
+
+    inputs = [
+        f"p {newcodefile.name}",
+        "s",
+        "a",
+        "3",
+        "00",
+        outputfile.name,
+        "e",
+        "r",
+        "",
+        "n",
+        "07  54",
+        "x",
+    ]
+
+    run_mvn('\n'.join(inputs))
+    
+    mvn_output = outputfile.read().replace('\n', '')
+    assert mvn_output == "61", \
+        f"Seu código não está correto\nConfira seu envio."
